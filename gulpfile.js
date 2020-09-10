0c356410c295958dcad209fed11d7fbc506a1891
andrav24 <andrav24@yandex.ru>
2020-09-10 13:06:38 +0700

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ let gulp = require('gulp'),
     cssmin = require('gulp-cssmin')
 
 gulp.task('sass', function () {
-  return gulp.src('app/scss/**/*.scss')
+  return gulp.src('app/scss/style.scss')
       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
       .pipe(rename({suffix: '.min'}))
       .pipe(autoprefixer({
@@ -22,7 +22,6 @@ gulp.task('style', function () {
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
-    'node_modules/magnific-popup/dist/magnific-popup.css'
   ])
       .pipe(concat('libs.min.css'))
       .pipe(cssmin())
@@ -31,8 +30,8 @@ gulp.task('style', function () {
 
 gulp.task('script', function () {
   return gulp.src([
-      'node_modules/slick-carousel/slick/slick.js',
-      'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
   ])
       .pipe(concat('libs.min.js'))
       .pipe(uglify())
